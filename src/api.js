eb2bd5a65d8b409669f01cc3d8aaf3dd50040996
S Manas <rockingmanas78@gmail.com>
2025-12-08 19:11:00 +0530

    Update API base URL for prod env

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'https://shipyardbackend-production.up.railway.app',
+  baseURL: 'https://shipyardbackend-production-b34e.up.railway.app',
   timeout: 0,  // 2 minutes
 });
 export default api;
