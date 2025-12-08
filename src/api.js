b85cf9b757e924a66d41a29de031039f4c291d79
Sarthak-Shreshtha01 <sarthakshreshtha345@gmail.com>
2025-12-08 23:59:28 +0530

    api change

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'http://localhost:5001',
+  // baseURL: 'http://localhost:5001',
+  baseURL: 'shipyardbackend-production.up.railway.app',
   timeout: 0,  // 2 minutes
 });
 export default api;
