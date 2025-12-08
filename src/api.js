6f3414e024a9ded9c0aa64e8ab6e3cb50f5d6ff7
S Manas <rockingmanas78@gmail.com>
2025-12-09 01:55:59 +0530

    Update API timeout comment for clarity

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 const api = axios.create({
   // baseURL: 'http://localhost:5001',
   baseURL: 'https://shipyardbackend-production.up.railway.app',
-  timeout: 0,  //2 minutes
+  timeout: 0, // 2 minutes
 });
 export default api;
